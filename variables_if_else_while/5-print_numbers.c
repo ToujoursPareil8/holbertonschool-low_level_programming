fbe1092c890d754aba747a9be1586f776c5e571f
jason <jasonaridy@gmail.com>
2025-10-22 10:21:17 +0000

    Everything is awesome, when you believe in your dreams

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - prints all single digit numbers of base 10 starting from 0
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+char digit;
+for (digit = '0'; digit <= '9'; digit++)
+{
+putchar(digit);
+}
+putchar('\n');
+return (0);
+}
