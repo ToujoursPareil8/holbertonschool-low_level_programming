3dc01548d096edd7d7092807521c646ddfa1dc8e
jason <jasonaridy@gmail.com>
2025-10-22 11:02:20 +0000

    print base 16 in lowercase

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Prints number of base 16 in lowercase
+ *
+ * Return: Always (0)
+ */
+
+int main(void)
+{
+int num;
+for (num = 0, num <=15, num ++)
+{
+putchar(num);
+;
+}
+putchar('\n');
+return (0);
+}
