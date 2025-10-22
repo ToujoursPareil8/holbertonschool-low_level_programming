7a339c73318871bda1f634dffd83b5bcd59907c1
jason <jasonaridy@gmail.com>
2025-10-22 10:25:32 +0000

    int instead of char

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - prints all single digit numbers of base 10 starting from 0
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+int digit;
+for (digit = '0'; digit <= '10'; digit++)
+{
+putchar(digit);
+}
+putchar('\n');
+return (0);
+}
