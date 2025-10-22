29b126b972de55c167e234addbb35ea6c97102ae
jason <jasonaridy@gmail.com>
2025-10-22 10:39:37 +0000

    int instead of char

--- variables_if_else_while/6-print_numberz.c
+++ variables_if_else_while/6-print_numberz.c
@@ -10,9 +10,9 @@ int main(void)
 int digit = 0;
 while (digit <= 9)
 {
-putchar(digit + '0');  // print digit as character
+putchar(digit + '0');
 digit++;
 }
-putchar('\n');  // print newline
+putchar('\n');
 return (0);
 }
