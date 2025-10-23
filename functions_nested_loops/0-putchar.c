09fb990ebc190b5414ab3a5e1aea0d6fdf057f32
jason <jasonaridy@gmail.com>
2025-10-23 11:35:19 +0000

    putchar again

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <unistd.h>
+#include "main.h"
+
+/**
+ * main - print _putchar to stdout
+ *
+ * Return always 0
+ */
+
+int main (void)
+{
+write(1,"_putchar\n", 9)
+return (0)
+}
