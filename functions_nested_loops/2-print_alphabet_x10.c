df94540859de2ed848067e0a618eb6ab36b662e0
jason <jasonaridy@gmail.com>
2025-10-24 08:28:40 +0000

    print the aphabet 10 times with while loops

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - imprime 10 fois l'alphabet en minuscule
+ */
+void print_alphabet_x10(void)
+{
+int i = 0;        /* Compte combien de fois on affiche l’alphabet */
+char ch;          /* Lettre actuelle */
+while (i < 10)
+{
+ch = 'a';     /* Commence à 'a' */
+while (ch <= 'z')
+{
+_putchar(ch);
+ch++;
+}
+_putchar('\n');  /* Nouvelle ligne après chaque alphabet */
+i++;
+}
+}
