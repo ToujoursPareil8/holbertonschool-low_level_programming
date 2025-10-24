542a6b08d95d70c2fe19a1ae0f721f5268242169
jason <jasonaridy@gmail.com>
2025-10-24 09:46:09 +0000

    sign

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -4,23 +4,23 @@
  *print_sign - print the sign of a number
  *@n: carac to test
  *
- * Return: 1 and print + if n > 0, 0 and print 0 if n is 0, -1 and print - if n is less than zero
+ * Return: 1 and print + if n > 0, 0 and print 0 if n is 0, else -1
  */
 int print_sign(int n)
 {
-if (n >0)
+if (n > 0)
 {
-_putchar ('+')
+_putchar ('+');
 return (1);
 }
 else if (n == 0)
 {
-_putchar ('0')
+_putchar ('0');
 return (0);
 }
-else 
+else
 {
-_putchar('-')
+_putchar('-');
 return (-);
 }
 }
