667574e49737b485d97b4cf8aa8104aea048c52d
jason <jasonaridy@gmail.com>
2025-10-28 13:36:14 +0000

    print number 0 to 9

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * mul - Multiplie deux entiers
+ * @a: premier entier
+ * @b: second entier
+ *
+ * Return: le produit de a et b
+ */
+void print_numbers(void)
+{
+int n = 0;
+while (n <= 9)
+{
+_putchar('0'+ n);
+n++;	
+_putchar('\n')
+}
