1c991adadebbf89ad2b00e794935e24362b98368
Jason <jasonaridy@gmail.com>
2025-11-02 15:26:26 +0000

    'o'

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <string.h>
 
 /**
  * cap_string - capitalizes all words of a string
@@ -9,17 +8,29 @@
  */
 char *cap_string(char *s)
 {
-    int i = 0;
+    int i, j;
     char sep[] = " \t\n,;.!?\"(){}";
 
-    while (s[i] != '\0')
+    for (i = 0; s[i] != '\0'; i++)
     {
-        if (i == 0 || strchr(sep, s[i - 1]))
+        // 1️⃣ Always check if current character starts a word
+        if (i == 0)
         {
             if (s[i] >= 'a' && s[i] <= 'z')
                 s[i] -= 32;
         }
-        i++;
+        else
+        {
+            // 2️⃣ Check if the previous char was a separator
+            for (j = 0; sep[j] != '\0'; j++)
+            {
+                if (s[i - 1] == sep[j] && s[i] >= 'a' && s[i] <= 'z')
+                {
+                    s[i] -= 32;
+                    break;
+                }
+            }
+        }
     }
 
     return (s);
