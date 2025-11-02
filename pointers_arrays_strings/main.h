7f9527927906dce37dbd5162c1137dc60bed11e1
Jason <jasonaridy@gmail.com>
2025-11-02 14:30:45 +0000

    idk

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -1,6 +1,14 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+char *leet(char *);
+char *cap_string(char *);
+char *string_toupper(char *);
+void reverse_array(int *a, int n);
+int _strcmp(char *s1, char *s2);
+char *_strncpy(char *dest, char *src, int n);
+char *_strncat(char *dest, char *src, int n);
+char *_strcat(char *dest, char *src);
 int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
