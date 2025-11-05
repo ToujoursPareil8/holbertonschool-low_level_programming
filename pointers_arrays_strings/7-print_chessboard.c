6255a4ec3d4c54397d45ad49dc0323dab9ea48ba
ToujoursPareil8 <jasonaridy@gmail.com>
2025-11-05 19:11:01 +0100

    feat add -print chessbboard prototype

---
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_chessboard - affiche un échiquier 8x8
+ * @a: tableau 2D de caractères représentant l'échiquier
+ *
+ * Chaque case est un caractère (par ex. 'r', 'n', 'b', 'q', 'k', 'p' ou
+ * un caractère quelconque). La fonction affiche chaque ligne de 8
+ * caractères puis un saut de ligne.
+ */
+
+ void print_chessboard(char (*a)[8])
+
+ {
+    int i, j;
+ }
