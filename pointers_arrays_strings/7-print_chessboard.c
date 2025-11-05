c896a0058e9007309d64d0431b50cc0f18130d69
ToujoursPareil8 <jasonaridy@gmail.com>
2025-11-05 19:13:00 +0100

    feat add -print chessbboard prototype

--- pointers_arrays_strings/7-print_chessboard.c
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -9,8 +9,18 @@
  * caractères puis un saut de ligne.
  */
 
- void print_chessboard(char (*a)[8])
+void print_chessboard(char (*a)[8])
 
- {
+{
     int i, j;
- }
+
+    for (i = 0; i < 8; i++)
+    {
+        for (j = 0; j < 8; j++)
+        {
+            _putchar(a[i][j]);
+        }
+        _putchar('\n');
+    }
+    
+}
