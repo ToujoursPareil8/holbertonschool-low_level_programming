5a007f09b1db6d57469a3b06a7ec368c68ee16ac
ToujoursPareil8 <jasonaridy@gmail.com>
2025-11-05 19:11:31 +0100

    feat add strstr prototype

--- pointers_arrays_strings/5-strstr.c
+++ pointers_arrays_strings/5-strstr.c
@@ -1,9 +1,11 @@
 #include "main.h"
 /**
- * 
- * 
- * 
- * 
+ * _strstr - locates a substring
+ * @haystack: string to search in
+ * @needle: substring to find
+ *
+ * Return: pointer to the beginning of the located substring,
+ * or NULL if the substring is not found
  */
 
 char *_strstr(char *haystack, char *needle)
