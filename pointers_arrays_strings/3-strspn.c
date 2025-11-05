88dbcdcffee787df74fb80a18c5f4b1680d9b4de
ToujoursPareil8 <jasonaridy@gmail.com>
2025-11-05 10:59:00 +0100

    feat add _strspn prototype

---
+++ pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * _strspn - gets the length of a prefix substring
+ * @s: string to search
+ * @accept: prefix to search for
+ *
+ * Return: number of bytes in the initial segment of s
+ *         which consist only of bytes from accept
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+    unsigned int count = 0;
+    char *a;
+    
+    for(; *s, s++;)
+    {
+        for (char a=accept; *a, a++;)
+        {
+            if (*s == *a)
+            {
+            count++;
+            break;
+            }
+        }
+    if ( *a == '\0' )
+    break;
+
+    return (count);
+    }
+}
