483c7faa3a94031b5ea25da54d2f0813d01d4627
ToujoursPareil8 <jasonaridy@gmail.com>
2025-11-05 19:52:31 +0100

    feat add prototype diagsums

--- pointers_arrays_strings/8-print_diagsums.c
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -1,5 +1,5 @@
-#include "main.h"
 #include <stdio.h>
+#include "main.h"
 
 /**
  * print_diagsums - prints the sum of the two diagonals of a square matrix
@@ -9,11 +9,13 @@
 
 void print_diagsums(int *a, int size)
  {
-    int i, sum1 = 0, sum2 = 0;
+    int i;
+    int sum1 = 0;
+    int sum2 = 0;
     for ( i = 0; i < size; i++)
     {
         sum1 += a[i * size + i];
-        sum2 += a[i * size + (size - 1 - i)]
+        sum2 += a[i * size + (size - 1 - i)];
     }
     printf("%d, %d\n", sum1, sum2);
  }
