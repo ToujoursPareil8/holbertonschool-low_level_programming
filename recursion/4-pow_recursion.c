9bcc932a2bdf2e164371c517d9c79c1a758f3a8c
ToujoursPareil8 <jasonaridy@gmail.com>
2025-11-06 14:26:53 +0100

    feat add _pow_recursion prototype

--- recursion/4-pow_recursion.c
+++ recursion/4-pow_recursion.c
@@ -1,5 +1,11 @@
 #include "main.h"
-
+/**
+ * _pow_recursion - returns the value of x raised to the power of y
+ * @x: base integer
+ * @y: exponent integer
+ *
+ * Return: the result of x raised to y, -1 if y is lower than 0
+ */
 _pow_recursion(int x, int y)
 {
     if(y == 0)
