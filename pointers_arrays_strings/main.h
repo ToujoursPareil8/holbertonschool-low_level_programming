6305c1598707776a42067fec0572e5c5a1293c50
ToujoursPareil8 <jasonaridy@gmail.com>
2025-11-07 09:40:53 +0100

    feat: add main.h

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -50,4 +50,11 @@ void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
 int _atoi(char *s);
 void print_rev(char *s);
+void _puts_recursion(char *s);
+void _print_rev_recursion(char *s);
+int _strlen_recursion(char *s);
+int factorial(int n);
+int _pow_recursion(int x, int y);
+int _sqrt_recursion(int n);
+int is_prime_number(int n);
 #endif
