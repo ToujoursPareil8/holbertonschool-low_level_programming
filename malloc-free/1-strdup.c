bc7036073993340bf4af9db4d40612a085ba1b6d
ToujoursPareil8 <jasonaridy@gmail.com>
2025-11-12 12:16:00 +0100

    feat: add prototypes all

--- malloc-free/1-strdup.c
+++ malloc-free/1-strdup.c
@@ -1,26 +1,32 @@
 #include <stdlib.h>
-#include <string.h>
 #include "main.h"
 
 /**
- * _strdup - returns a pointer to a newly allocated copy of a string
- * @str: string to duplicate
+ * _strdup - renvoie un pointeur vers une nouvelle chaîne de caractères,
+ *  copie de la chaîne `str`. La mémoire est allouée
+ *  @str chaîne source
  *
- * Return: pointer to duplicated string, or NULL if str is NULL or malloc fails
+ *  Return: pointeurvers la nouvelle chaîne, ou NULL si erreur
  */
-char *_strdup(char*str)
+char *_strdup(char *str)
 {
-    char *dup;
+	char *buffer;
+	unsigned int i, size;
 
-    if (str == NULL)
-    {
-        return(NULL);
-    }
-    dup = malloc(strlen(str) + 1);
-    if (dup == NULL)
-    {
-        return(NULL);
-    }
-    strcpy(dup, str);
-    return(dup);
+	if (str == NULL)
+		return(NULL);
+
+	for (size = 0; str[size] != '\0'; size++)
+		;
+
+	buffer = malloc(sizeof(char) * (size + 1));
+	if (buffer == NULL)
+		return(NULL);
+
+	for (i = 0; i < size; i++)
+		buffer[i] = str[i];
+	buffer[i] = '\0';
+
+
+	return(buffer);
 }
