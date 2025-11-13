b028e1419d0247b13622d52ee6f07e702a4fd094
ToujoursPareil8 <jasonaridy@gmail.com>
2025-11-13 20:04:04 +0100

    feat: add prorotype of array range

---
+++ more_malloc_free/3-array_range.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+
+/**
+ * array_range - creates an array of integers from min to max
+ * @min: starting value
+ * @max: ending value
+ *
+ * Return: pointer to the array, or NULL if min > max or malloc fails
+ */
+int *array_range(int min, int max)
+{
+int *arr;
+int i;
+
+if (min > max)
+return (NULL);
+arr = malloc((max - min + 1) * sizeof(int));
+if (arr == NULL)
+return (NULL);
+for (i = 0; i <= (max - min); i++)
+arr[i] = min + i;
+return (arr);
+}
