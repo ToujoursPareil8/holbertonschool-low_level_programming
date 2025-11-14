e7dc6cbec5e6265491b4f76507baabf97427090a
ToujoursPareil8 <jasonaridy@gmail.com>
2025-11-14 19:29:38 +0100

    feat: dogs are wild beasts

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -1,20 +1,22 @@
+#include <stddef.h>
 #include "dog.h"
 
 /**
- * init_dog - initializes a variable of type struct dog
- * @d: pointer to struct dog to initialize
- * @name: name of the dog
- * @age: age of the dog
- * @owner: owner of the dog
+ * init_dog - initialise une variable de type structure chien
+ * @d: pointeur de la structure chien à initialiser
+ * @name: nom du chien
+ * @age: âge du chien
+ * @owner: propriétaire du chien
  *
- * Return: nothing
+ * Description: Initialise le nom, l'âge et le propriétaire
+ * d'une structure de type dog. Si d est NULL, la fonction ne fait rien.
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-    if (d == NULL)
-        return;
+	if (d == NULL)
+		return;
 
-    d->name = name;
-    d->age = age;
-    d->owner = owner;
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
 }
