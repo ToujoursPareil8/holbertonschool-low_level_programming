8aa3eb8737b5231c168250156949259de173fafc
ToujoursPareil8 <jasonaridy@gmail.com>
2025-11-14 19:26:15 +0100

    feat: dogs are wild beasts

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -2,17 +2,23 @@
 #define DOG_H
 
 /**
- * struct dog - structure décrivant un chien
- * @name: nom du chien
- * @age: âge du chien
- * @owner: propriétaire du chien
+ * struct dog - structure describing a dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
  */
 struct dog
 {
-char *name;
-float age;
-char *owner;
+    char *name;
+    float age;
+    char *owner;
 };
+
+typedef struct dog dog_t;
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 
 #endif /* DOG_H */
