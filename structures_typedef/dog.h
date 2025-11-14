7c3cf479fc0c60e1fc47714e70978459cedeee59
ToujoursPareil8 <jasonaridy@gmail.com>
2025-11-14 19:05:04 +0100

    feat: add dog.h

---
+++ structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - structure décrivant un chien
+ * @name: nom du chien
+ * @age: âge du chien
+ * @owner: propriétaire du chien
+ */
+struct dog
+{
+char *name;
+float age;
+char *owner;
+};
+
+#endif
