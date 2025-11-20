5b674002e0f845c493afa276261893a335eb6116
ToujoursPareil8 <jasonaridy@gmail.com>
2025-11-20 19:42:05 +0100

    feat: added a function to get the operator for the next function pointer

--- function_pointers/3-get_op_func.c
+++ function_pointers/3-get_op_func.c
@@ -24,9 +24,7 @@ int (*get_op_func(char *s))(int, int)
     while (ops[i].op != NULL)
     {
         if (strcmp(s, ops[i].op) == 0)
-        {
             return(ops[i].f);
-        }
         i++;
     }
     return (NULL);
