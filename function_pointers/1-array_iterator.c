f3162b897f0f5ebfccc8109c4a1311f3a350e29c
ToujoursPareil8 <jasonaridy@gmail.com>
2025-11-20 10:30:20 +0100

    feat: added a functions that execute a function on each element of an array

--- function_pointers/1-array_iterator.c
+++ function_pointers/1-array_iterator.c
@@ -6,15 +6,18 @@
 * @size: size of the array
 * @action: pointer to function to apply
 *
-* Return: void
+* Return: nothing
 */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
 size_t i;
 
-if (array == NULL || action == NULL)
+if (array == NULL || action == NULL )
+{
 return;
-
+}
 for (i = 0; i < size; i++)
+{
 action(array[i]);
+}
 }
