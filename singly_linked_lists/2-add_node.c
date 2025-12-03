96b44cf78ec706ed09b3bc73c28a516ae4e0c2d8
ToujoursPareil8 <jasonaridy@gmail.com>
2025-12-03 11:48:28 +0100

    feat: added  add node function

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -1,43 +1,55 @@
 #include "lists.h"
-#include <stdlib.h>
 #include <string.h>
+#include <stdlib.h>
 
 /**
- * add_node - adds a new node at the beginning of a list_t list
- * @head: pointer to pointer to head of list
- * @str: string to duplicate into the new node
+ * add_node - Adds a new node at the beginning of a list_t list.
+ * @head: A pointer to a pointer to the head of the list.
+ * @str: The string to be duplicated and stored in the new node.
  *
- * Return: address of the new element, or NULL if it fails
+ * This function allocates a new node, duplicates the input string (and
+ * calculates its length), sets the new node's next pointer to the current
+ * head, and finally updates the head pointer to the new node.
+ *
+ * Return: The address of the new element (node), or NULL if it failed.
  */
 list_t *add_node(list_t **head, const char *str)
 {
-	list_t *new;
-	unsigned int len = 0;
+    list_t *new_node;
+    char *dup_str;
+    unsigned int len = 0;
 
-	new = malloc(sizeof(list_t));
-	if (new == NULL)
-		return (NULL);
+    /* 1. Calculate string length */
+    if (str == NULL)
+        return (NULL); /* String must not be NULL for duplication/length */
 
-	if (str)
-	{
-		new->str = strdup(str);
-		if (new->str == NULL)
-		{
-			free(new);
-			return (NULL);
-		}
+    while (str[len])
+        len++;
 
-		while (str[len])
-			len++;
-	}
-	else
-	{
-		new->str = NULL;
-	}
+    /* 2. Allocate memory for the new node */
+    new_node = malloc(sizeof(list_t));
+    if (new_node == NULL)
+        return (NULL);
 
-	new->len = len;
-	new->next = *head;
-	*head = new;
+    /* 3. Duplicate the string using strdup */
+    dup_str = strdup(str);
+    if (dup_str == NULL)
+    {
+        /* Clean up node memory if string duplication fails */
+        free(new_node);
+        return (NULL);
+    }
 
-	return (new);
+    /* 4. Populate the new node */
+    new_node->str = dup_str;
+    new_node->len = len;
+
+    /* 5. Link the new node to the current head (insert at beginning) */
+    new_node->next = *head;
+
+    /* 6. Update the list head to point to the new node */
+    *head = new_node;
+
+    /* 7. Return the address of the new node */
+    return (new_node);
 }
