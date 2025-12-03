e383ef705e83c4466055268f5c5e764ea704ae54
ToujoursPareil8 <jasonaridy@gmail.com>
2025-12-03 12:01:47 +0100

    feat: added  add node function

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -1,76 +1,33 @@
 #include "lists.h"
 #include <stdlib.h>
-
-/**
- * _strlen - returns length of a string
- * @s: string
- * Return: length
- */
-unsigned int _strlen(const char *s)
-{
-	unsigned int i = 0;
-
-	if (s == NULL)
-		return (0);
-
-	while (s[i])
-		i++;
-
-	return (i);
-}
-
-/**
- * _strcpy - copies a string
- * @dest: destination
- * @src: source
- * Return: dest
- */
-char *_strcpy(char *dest, const char *src)
-{
-	unsigned int i = 0;
-
-	if (src == NULL)
-	{
-		dest[0] = '\0';
-		return (dest);
-	}
-
-	while (src[i])
-	{
-		dest[i] = src[i];
-		i++;
-	}
-	dest[i] = '\0';
-
-	return (dest);
-}
+#include <string.h>
 
 /**
  * add_node - adds a new node at the beginning of a list_t list
- * @head: pointer to pointer to head
- * @str: string to duplicate into node
+ * @head: pointer to pointer to head of list
+ * @str: string to duplicate
  *
- * Return: address of new element, or NULL on failure
+ * Return: address of new element, or NULL if it fails
  */
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new;
-	unsigned int len;
+	unsigned int len = 0;
 
 	new = malloc(sizeof(list_t));
 	if (new == NULL)
 		return (NULL);
 
-	len = _strlen(str);
-
-	new->str = malloc(len + 1);
+	new->str = strdup(str);
 	if (new->str == NULL)
 	{
 		free(new);
 		return (NULL);
 	}
 
-	_strcpy(new->str, str);
+	while (str[len])
+		len++;
+
 	new->len = len;
 	new->next = *head;
 	*head = new;
