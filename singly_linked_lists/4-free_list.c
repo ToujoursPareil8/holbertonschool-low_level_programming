cc81221f8e1d51c27f2ad82afbdbbace4d51c0b6
ToujoursPareil8 <jasonaridy@gmail.com>
2025-12-05 21:23:53 +0100

    added free list function

--- singly_linked_lists/4-free_list.c
+++ singly_linked_lists/4-free_list.c
@@ -1,19 +1,19 @@
 #include "lists.h"
-#include <stdio.h>
+#include <stdlib.h>
+
 /**
- * free_list - Frees a list_t list
- * @head: Pointer to the list_t list to be freed
+ * free_list - frees a list_t list
+ * @head: pointer to the first node of the list
  */
 void free_list(list_t *head)
 {
-	list_t *temp;
+    list_t *temp;
 
-	while (head != NULL)
-	{
-		temp = head->next;
-		free(head->str);
-
-		free(head);
-		head = temp;
-	}
+    while (head != NULL)
+    {
+        temp = head->next; /* Save next node */
+        free(head->str);   /* Free malloc'ed string */
+        free(head);        /* Free the node itself */
+        head = temp;       /* Move to next node */
+    }
 }
