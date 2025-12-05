db5576664e4d57bf14cfcce25c197f9f6a787009
ToujoursPareil8 <jasonaridy@gmail.com>
2025-12-05 21:19:15 +0100

    added free list function

--- singly_linked_lists/4-free_list.c
+++ singly_linked_lists/4-free_list.c
@@ -10,16 +10,10 @@ void free_list(list_t *head)
 
 	while (head != NULL)
 	{
-		/* 1. On sauvegarde l'adresse du prochain nœud */
 		temp = head->next;
-
-		/* 2. On libère la chaine de caractères (allouée par strdup) */
 		free(head->str);
 
-		/* 3. On libère le nœud lui-même */
 		free(head);
-
-		/* 4. On passe au suivant grâce à la sauvegarde */
 		head = temp;
 	}
 }
